fb0fad4489d13ceba1ef587432d02224db2aeba6
Guru Prakash Ram <69163711+guru-apr21@users.noreply.github.com>
2020-11-30 12:23:07 +0530

    Create form, handleSubmit and set state using ref.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Container, Form, Button } from 'react-bootstrap';
 
-const Login = () => {
-  return <div></div>;
+const Login = ({ onIdSubmit }) => {
+  const idRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onIdSubmit(idRef.current.value);
+  };
+
+  return (
+    <Container
+      className="align-items-center d-flex"
+      style={{ height: '100vh' }}
+    >
+      <Form className="w-100" onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Label>Enter your Id</Form.Label>
+          <Form.Control type="text" ref={idRef} required></Form.Control>
+        </Form.Group>
+        <Button type="submit" className="mr-2">
+          Login
+        </Button>
+        <Button variant="secondary">Create A new Id</Button>
+      </Form>
+    </Container>
+  );
 };
 
 export default Login;
